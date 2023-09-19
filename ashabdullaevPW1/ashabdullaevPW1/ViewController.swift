0ec16d1ada7aa8b47778c989f6ff8d8f601b515b
aub (Аюбхон) <aubhon.a@gmail.com>
2023-09-19 20:42:23 +0300

    The first home task

--- ashabdullaevPW1/ashabdullaevPW1/ViewController.swift
+++ ashabdullaevPW1/ashabdullaevPW1/ViewController.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 
+// MARK: - ViewController
+
 class ViewController: UIViewController {
+    
     @IBOutlet var views: [UIView]!
     @IBOutlet weak var button: UIButton!
+    
+    // MARK: View loader
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +22,21 @@ class ViewController: UIViewController {
         changeBackgroundColor(uniqueColors: getUniqueColors())
     }
     
+    // MARK: Pressing button handler
+    
     @IBAction func buttonWasPressed(_ sender: UIButton) {
         let uniqueColors = getUniqueColors();
-        UIView.animate(withDuration: 3.49, animations: {
+        let animationDuration = 3.49
+        UIView.animate(withDuration: animationDuration, animations: {
             self.button.isEnabled = false
             self.changeBackgroundColor(uniqueColors: uniqueColors)
             self.changeCornerRadius()
         }, completion: { [weak self] _ in
             self?.button.isEnabled = true
-        }
-        )
+        })
     }
+    
+    // MARK: Fuction change view's corner radius
     
     func changeCornerRadius() {
         for view in views {
@@ -35,11 +44,15 @@ class ViewController: UIViewController {
         }
     }
     
+    // MARK: Fuction change view's background color
+    
     func changeBackgroundColor(uniqueColors: [UIColor]) {
-        for i in 0..<views.count {
+        for i in 0 ..< views.count {
             views[i].backgroundColor = uniqueColors[i]
         }
     }
+    
+    // MARK: Function generate array of unique UIColor
     
     func getUniqueColors() -> [UIColor] {
         var uniqueColors = [UIColor]()
@@ -55,30 +68,31 @@ class ViewController: UIViewController {
 
 extension UIColor {
     
-    // MARK: - init convert hex color to RGB
+    // MARK: init convert hex color to RGB
     
     convenience init(hexColor : String) {
         let hexColorChanged = hexColor.replacingOccurrences(of: "#", with: "")
-        
-        let rgb = UInt64(hexColorChanged, radix: 16)!
-        
-        let red = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-        let green = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-        let blue = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-        let alpha = CGFloat(rgb & 0x000000FF) / 255.0
+        let base = 16
+        let rgb = UInt64(hexColorChanged, radix: base)!
+        let maxOfDiapValue = 255.0
+        let red = CGFloat((rgb & 0xFF000000) >> 24) / maxOfDiapValue
+        let green = CGFloat((rgb & 0x00FF0000) >> 16) / maxOfDiapValue
+        let blue = CGFloat((rgb & 0x0000FF00) >> 8) / maxOfDiapValue
+        let alpha = CGFloat(rgb & 0x000000FF) / maxOfDiapValue
         
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
     
-    // MARK: - Random hex color generator
+    // MARK: Random hex color generator
     
     static func generateRandomHexColor() -> String {
         var hexColor = "#"
-        for _ in 0..<3 {
-            let randomHex = String(Int.random(in: 0...255), radix: 16)
+        let base = 16
+        for _ in 0 ..< 3 {
+            let randomHex = String(Int.random(in: 0...255), radix: base)
             hexColor += randomHex.count == 1 ? "0" + randomHex : randomHex
         }
-        hexColor += String(Int.random(in: 128...255), radix: 16)
+        hexColor += String(Int.random(in: 128...255), radix: base)
         return hexColor
     }
 }
